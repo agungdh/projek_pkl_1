2d30b32ab1d487c1d14ec26124888e93a48e1a57
agungdh <agungdh@live.com>
2018-02-21 11:37:16 +0700

    apus gapenting

--- application/views/lihat_nilai/index.php
+++
@@ -1,4 +0,0 @@
-<form method="post">
-	NIS : <input type="text" name="nis" value="<?php echo $this->input->post('nis') != null ? $this->input->post('nis') : null; ?>">
-	<input type="submit" name="submit" value="Submit">
-</form>
